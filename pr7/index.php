e6561468c78bbba4fa2ecce0fd1e5c89c8509614
Piotr Bauer <65217796+Bauero@users.noreply.github.com>
2023-04-17 23:27:22 +0200

    correction in insert into

--- pr7/index.php
+++ pr7/index.php
@@ -5,7 +5,8 @@
   <title>My Database</title>
 </head>
 <body>
-  <h1>My Database</h1>
+  <h1>Piotr Bauer</h1>
+  <h3>Projekt 7 - Baza danych</h3>
   <?php
      session_start();
 
@@ -27,7 +28,7 @@
            $id = $linia[0];
            echo "
            <form action='' method='POST'>
-           <input type='submit' value='$id' name='usun' id=$id></input>
+           <input type='submit' value='$id' name='usun' id=$id style='border: solid red 0.3em' title='Usun rekord $id - $linia[1] $linia[2] $linia[3]'></input>
            <p>$linia[1] $linia[2] $linia[3]</p>
            </form>";
            echo "<hr>";
@@ -44,7 +45,7 @@
         <input type='text' name='imie' id='imie'></input>
         <input type='text' name='nazwisko' id='nazwisko'></input>
         <input type='text' name='wiek' id='wiek'></input>
-        <input type='submit' value='Wpisz' name='Wpisz' id=$id></input>
+        <input type='submit' value='Wpisz' name='Wpisz' id=$id style='border: solid blue 0.3em' title='Wpisz osobę o tych danych'></input>
         </form>";
       unset($_SESSION['dodaj']);
       exit();
@@ -52,15 +53,15 @@
     
     else if(isset($_POST['Wpisz'])){
       echo "Wykonanie wpisu";
-      $imie = $_POST['imie'];
-      $nazwisko= $_POST['nazwisko'];
-      $wiek= $_POST['wiek'];
-      $dane = "INSERT INTO `osoby` (`id`, `imie`, `nazwisko`, `wiek`) VALUES (NULL, \'Witold\', \'Gombrowicz\', \'53\'), (NULL, \'Karolina\', \'Witawa\', \'23\')";
+      $name= $_POST['imie'];
+      $surname= $_POST['nazwisko'];
+      $age= $_POST['wiek'];
+      $dane = "INSERT INTO `osoby` (`id`, `imie`, `nazwisko`, `wiek`) VALUES (NULL, $name, $surname, $age)";
       $res = $mysqli -> query($dane);
       echo $res;
-      unset($_SESSION['imie']);
-      unset($_SESSION['nazwisko']);
-      unset($_SESSION['wiek']);
+      unset($_SESSION['name']);
+      unset($_SESSION['surname']);
+      unset($_SESSION['age']);
       unset($_POST['dodane']);
       unset($_POST['Wpisz']);
       
@@ -75,14 +76,14 @@
            $id = $linia[0];
            echo "
            <form action='' method='POST'>
-           <input type='submit' value='$id' name='usun' id=$id></input>
+           <input type='submit' value='$id' name='usun' id=$id style='border: solid red 0.3em' title='Usun rekord $id - $linia[1] $linia[2] $linia[3]'></input>
            <p>$linia[1] $linia[2] $linia[3]</p>
            </form>";
            echo "<hr>";
         }
         
         echo "<form action='' method='POST'>
-          <input type='submit' value='Dodaj' name='dodaj'></input></form>";
+          <input type='submit' value='Dodaj' name='dodaj'  style='border: solid green 0.3em' title='Dodaj nową osobę do bazy'></input></form>";
     $mysqli -> close();
     ?>
   <pre>
@@ -131,15 +132,17 @@ session_start();
     
     else if(isset($_POST['Wpisz'])){
       echo "Wykonanie wpisu";
-      $imie = $_POST['imie'];
-      $nazwisko= $_POST['nazwisko'];
-      $wiek= $_POST['wiek'];
-      $dane = "INSERT INTO `osoby` (`id`, `imie`, `nazwisko`, `wiek`) VALUES (NULL, \'Witold\', \'Gombrowicz\', \'53\'), (NULL, \'Karolina\', \'Witawa\', \'23\')";
+      $name= $_POST['imie'];
+      $surname= $_POST['nazwisko'];
+      $age= $_POST['wiek'];
+      $dane = "INSERT INTO `osoby` (`id`, `imie`, `nazwisko`, `wiek`) VALUES (NULL, $name, $surname, $age)";
       $res = $mysqli -> query($dane);
       echo $res;
-      unset($_SESSION['imie']);
-      unset($_SESSION['nazwisko']);
-      unset($_SESSION['wiek']);
+      unset($_SESSION['name']);
+      unset($_SESSION['surname']);
+      unset($_SESSION['age']);
+      unset($_POST['dodane']);
+      unset($_POST['Wpisz']);
       unset($_POST['dodane']);
       unset($_POST['Wpisz']);
       
