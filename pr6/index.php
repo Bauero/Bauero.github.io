a7daa683a156d48dfc87a759aeaa9954c4428c47
Piotr Bauer <65217796+Bauero@users.noreply.github.com>
2023-04-17 23:19:06 +0200

    buttons coloring

--- pr6/index.php
+++ pr6/index.php
@@ -1,81 +1,168 @@
-<?php
-    
-    session_start();
-    
-    if(isset($_POST["logowanie"]))
-    { 
-       
-        $login=$_POST["login"];
-        $haslo=$_POST["haslo"];
-        $dobrylogin="admin";
-        $dobrehaslo="admin";
-
-        if($dobrylogin==$login && $dobrehaslo==$haslo)
-        {   
-            // dzięki temu sprawdzam czy jeszcze mogę wejść
-            $_SESSION['logged']=true;
-            header("Location: formularz.php");
-        }
-    }
-   
-?>
 
 <!DOCTYPE html>
-<html lang="pl">
+<html>
 <head>
-    <title>Logowanie</title>
-    <meta charset="utf-8">
-    
-    <link rel="stylesheet" href="style.css" />
-
+  <title>My Database</title>
 </head>
 <body>
+  <h1>My Database</h1>
+  <?php
+     session_start();
 
-    <div>
-        <div class="wycentrowane"><h1>Logowanie</h1></div>
-
-        <br>
-            <form method="post" action="">
-                <h3>Login</h3><br>
-                <input type="text" name="login" required>
-
-                <br>
-                <br>
-
-                <h3>Hasło</h3><br>
-                <input  type="password" name="haslo" required>
-
-                <br>
-                <br>
-
-                <button type="submit" name="logowanie">Zaloguj</button>
-            </form> 
-            <h3>login & hasło = "admin"</h3>
-            
-        <pre>
-            
-        session_start();
-        
-        if(isset($_POST["logowanie"]))
-        { 
-           
-            $login=$_POST["login"];
-            $haslo=$_POST["haslo"];
-            $dobrylogin="admin";
-            $dobrehaslo="admin";
+    $mysqli = new mysqli("mysql.ct8.pl","m35167_Bauero","Admin_123","m35167_Bauero");  
     
-            if($dobrylogin==$login && $dobrehaslo==$haslo)
-            {   
-                // dzięki temu sprawdzam czy jeszcze mogę wejść
-                $_SESSION['logged']=true;
-                header("Location: formularz.php");
-            }
+    if($_POST['usun'])
+    {
+      $value = $_POST['usun'];
+      $mysqli->query("DELETE FROM osoby WHERE id=$value");
+      unset($_SESSION['usun']);
+      
+      $sql = "SELECT * FROM osoby";
+        $result = $mysqli -> query($sql);
+
+        // Retrieve records from the database
+      
+        while($linia= $result -> fetch_array(MYSQLI_NUM))
+        {
+           $id = $linia[0];
+           echo "
+           <form action='' method='POST'>
+           <input type='submit' value='$id' name='usun' id=$id style='border: solid red 0.3em' title='Usun rekord $id - $linia[1] $linia[2] $linia[3]'></input>
+           <p>$linia[1] $linia[2] $linia[3]</p>
+           </form>";
+           echo "<hr>";
+        }
+        
+        echo "<form action='' method='POST'>
+          <input type='submit' value='dodaj'></input></form>";
+      exit();
+    }
+    
+    else if(isset($_POST['dodaj'])){
+      echo "
+        <form action='' method='POST'>
+        <input type='text' name='imie' id='imie'></input>
+        <input type='text' name='nazwisko' id='nazwisko'></input>
+        <input type='text' name='wiek' id='wiek'></input>
+        <input type='submit' value='Wpisz' name='Wpisz' id=$id style='border: solid blue 0.3em' title='Wpisz osobę o tych danych'></input>
+        </form>";
+      unset($_SESSION['dodaj']);
+      exit();
+      }
+    
+    else if(isset($_POST['Wpisz'])){
+      echo "Wykonanie wpisu";
+      $imie = $_POST['imie'];
+      $nazwisko= $_POST['nazwisko'];
+      $wiek= $_POST['wiek'];
+      $dane = "INSERT INTO `osoby` (`id`, `imie`, `nazwisko`, `wiek`) VALUES (NULL, \'Witold\', \'Gombrowicz\', \'53\'), (NULL, \'Karolina\', \'Witawa\', \'23\')";
+      $res = $mysqli -> query($dane);
+      echo $res;
+      unset($_SESSION['imie']);
+      unset($_SESSION['nazwisko']);
+      unset($_SESSION['wiek']);
+      unset($_POST['dodane']);
+      unset($_POST['Wpisz']);
+      
+      }
+    $sql = "SELECT * FROM osoby";
+        $result = $mysqli -> query($sql);
+
+        // Retrieve records from the database
+      
+        while($linia= $result -> fetch_array(MYSQLI_NUM))
+        {
+           $id = $linia[0];
+           echo "
+           <form action='' method='POST'>
+           <input type='submit' value='$id' name='usun' id=$id style='border: solid red 0.3em' title='Usun rekord $id - $linia[1] $linia[2] $linia[3]'></input>
+           <p>$linia[1] $linia[2] $linia[3]</p>
+           </form>";
+           echo "<hr>";
+        }
+        
+        echo "<form action='' method='POST'>
+          <input type='submit' value='Dodaj' name='dodaj'  style='border: solid green 0.3em' title='Dodaj nową osobę do bazy'></input></form>";
+    $mysqli -> close();
+    ?>
+  <pre>
+session_start();
+
+    $mysqli = new mysqli("mysql.ct8.pl","m35167_Bauero","Admin_123","m35167_Bauero");  
+    
+    if($_POST['usun'])
+    {
+      $value = $_POST['usun'];
+      $mysqli->query("DELETE FROM osoby WHERE id=$value");
+      unset($_SESSION['usun']);
+      
+      $sql = "SELECT * FROM osoby";
+        $result = $mysqli -> query($sql);
+
+        // Retrieve records from the database
+      
+        while($linia= $result -> fetch_array(MYSQLI_NUM))
+        {
+           $id = $linia[0];
+           echo "
+           form action='' method='POST'>
+           input type='submit' value='$id' name='usun' id=$id>/input>
+           p>$linia[1] $linia[2] $linia[3]/p>
+           /form>";
+           echo "hr>";
         }
        
-        </pre>
-    </div>
+        echo "form action='' method='POST'>
+          input type='submit' value='dodaj'>/input>/form>";
+      exit();
+    }
+    
+    else if(isset($_POST['dodaj'])){
+      echo "
+        form action='' method='POST'>
+        input type='text' name='imie' id='imie'>/input>
+        nput type='text' name='nazwisko' id='nazwisko'>/input>
+        input type='text' name='wiek' id='wiek'>/input>
+        input type='submit' value='Wpisz' name='Wpisz' id=$id>/input>
+        /form>";
+      unset($_SESSION['dodaj']);
+      exit();
+      }
+    
+    else if(isset($_POST['Wpisz'])){
+      echo "Wykonanie wpisu";
+      $imie = $_POST['imie'];
+      $nazwisko= $_POST['nazwisko'];
+      $wiek= $_POST['wiek'];
+      $dane = "INSERT INTO `osoby` (`id`, `imie`, `nazwisko`, `wiek`) VALUES (NULL, \'Witold\', \'Gombrowicz\', \'53\'), (NULL, \'Karolina\', \'Witawa\', \'23\')";
+      $res = $mysqli -> query($dane);
+      echo $res;
+      unset($_SESSION['imie']);
+      unset($_SESSION['nazwisko']);
+      unset($_SESSION['wiek']);
+      unset($_POST['dodane']);
+      unset($_POST['Wpisz']);
+      
+      }
+    $sql = "SELECT * FROM osoby";
+        $result = $mysqli -> query($sql);
 
-
-
+        // Retrieve records from the database
+      
+        while($linia= $result -> fetch_array(MYSQLI_NUM))
+        {
+           $id = $linia[0];
+           echo "
+           form action='' method='POST'>
+           input type='submit' value='$id' name='usun' id=$id>/input>
+           p>$linia[1] $linia[2] $linia[3]/p>
+           /form>";
+           echo "hr>";
+        }
+        
+        echo "form action='' method='POST'>
+          input type='submit' value='Dodaj' name='dodaj'>/input>/form>";
+    $mysqli -> close();
+</pre>
 </body>
 </html>
